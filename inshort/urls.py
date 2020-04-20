11b29c2cf893667197749faf3aaba2c0e41789a9
Aleksandr Klochkov <klochkov1w@gmail.com>
2020-04-20 23:08:58 +0300

    home renamed

--- inshort/urls.py
+++ inshort/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('mainapp.urls')),
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
