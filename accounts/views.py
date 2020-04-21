1c87a0054ce64bd924e21635b26b4378c40a82fb
Vlad <amurahovskiy@gmail.com>
2020-04-21 21:15:07 +0300

    Add auth in project

---
+++ accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.template import loader
+from django.views.generic import TemplateView
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class LoginView(TemplateView):
+    template_name = "registration/login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        context = {}
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                context['error'] = "Логин или пароль неправильные"
+        return render(request, self.template_name, context)
+class RegisterView(TemplateView):
+    template_name = "registration/register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            password2 = request.POST.get('password2')
+
+            if password == password2:
+                User.objects.create_user(username, email, password)
+                return redirect(reverse("login"))
+
+        return render(request, self.template_name)
--- inshort/urls.py
+++ inshort/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('', include('custom_urls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
