649b0418aaefb40ecc058a0a0ed0d18da1099b5e
Aleksandr Klochkov <klochkov1w@gmail.com>
2020-04-26 02:49:03 +0300

    Logout added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import views
 from django.urls import path
-from accounts.views import LoginView, RegisterView
+from accounts.views import LoginView, RegisterView, logout_user
  
 urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
-    path('register/', RegisterView.as_view(), name="register"),]
+    path('register/', RegisterView.as_view(), name="register"),
+    path('logout/', logout_user, name="logout")]
