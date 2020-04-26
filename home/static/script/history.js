3bd1907bcf953f8ca96796d0b9dd3d11f075a76e
Vlad <amurahovskiy@gmail.com>
2020-04-26 20:55:52 +0300

    Add fix with history and header in mainPage

---
+++ home/static/script/history.js
@@ -0,0 +1,31 @@
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    this.multiple = multiple || false;
+
+    var dropdownlink = this.el.find('.dropdownlink');
+    dropdownlink.on('click',
+      { el: this.el, multiple: this.multiple },
+      this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el,
+      $this = $(this),
+      $next = $this.next();
+      console.log(5);
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+    if (!e.data.multiple) {
+      $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+  var accordion = new Accordion($('.accordion-menu'), false);
+  
+  load_user_urls()
+});
+
+
+function load_user_urls() {
+  $("#urls_container").load('/urls/my')
+}
