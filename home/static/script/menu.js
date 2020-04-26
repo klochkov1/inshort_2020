7740fbd832848de3c37bf37408c1810b1279f002
Vlad <amurahovskiy@gmail.com>
2020-04-26 18:41:20 +0300

    Add page FAQ,Login,

--- home/static/script/menu.js
+++ home/static/script/menu.js
@@ -27,14 +27,10 @@ $(function() {
 var btn = document.querySelector(".btn_modal_window");
 console.log(btn);
 var span = document.querySelector(".close_modal_window");
-
-
 console.log(btn);
 btn.addEventListener('click', function () {
    modal.style.display = "block";
 })
-
-
 span.addEventListener('click', function () {
    modal.style.display = "none";
 })
