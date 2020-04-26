5e73f56c823f1f62f37eb02ef7a457088e298a1b
Vlad <amurahovskiy@gmail.com>
2020-04-26 17:41:21 +0300

    Add model window for input parameters,refackt code HTML,CSS and change menu

--- home/static/script/menu.js
+++ home/static/script/menu.js
@@ -23,3 +23,24 @@ $(function() {
     }
     var accordion = new Accordion($('.accordion-menu'), false);
   })
+  var modal = document.querySelector(".modal");
+var btn = document.querySelector(".btn_modal_window");
+console.log(btn);
+var span = document.querySelector(".close_modal_window");
+
+
+console.log(btn);
+btn.addEventListener('click', function () {
+   modal.style.display = "block";
+})
+
+
+span.addEventListener('click', function () {
+   modal.style.display = "none";
+})
+
+window.onclick = function (event) {
+   if (event.target == modal) {
+       modal.style.display = "none";
+   }
+}
